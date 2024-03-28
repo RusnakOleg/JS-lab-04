8b3b132c657618e8b71fc4fd93d30e516615e4f7
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-03-28 23:18:41 +0200

    task 1.3

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ function filterOddElements(arr) {
 
 var output = filterOddElements([1,2,3,4,5]);
 console.log(output);
+
+//1.3
+function getLengthOfShortestElement(arr) {
+    if (arr.length === 0) {
+        return 0; 
+    }
+
+    var shortestLength = arr[0].length; 
+
+    for (var i = 1; i < arr.length; i++) {
+        var currentLength = arr[i].length;
+        if (currentLength < shortestLength) {
+            shortestLength = currentLength; 
+        }
+    }
+
+    return shortestLength;
+}
+
+var output = getLengthOfShortestElement(["one", "two", "three"]);
+console.log(output);
