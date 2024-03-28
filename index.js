39eeaefc8f482ead8b0576a1385ffeaefec41770
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-03-28 23:33:01 +0200

    task 1.7

--- index.js
+++ index.js
@@ -76,3 +76,24 @@ function computeSummationToN(n) {
 
 var output = computeSummationToN(6);
 console.log(output);
+
+// 1.7
+function convertScoreToGrade(score) {
+    switch (true) {
+        case (score >= 90 && score <= 100):
+            return 'A';
+        case (score >= 80 && score < 90):
+            return 'B';
+        case (score >= 70 && score < 80):
+            return 'C';
+        case (score >= 60 && score < 70):
+            return 'D';
+        case (score >= 0 && score < 60):
+            return 'F';
+        default:
+            return 'INVALID SCORE';
+    }
+}
+
+var output = convertScoreToGrade(91);
+console.log(output);
