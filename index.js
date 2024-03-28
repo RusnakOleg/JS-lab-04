ae6abb87858fa8ad1058b02d0cdb96718c395522
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-03-28 23:35:22 +0200

    task 1.8

--- index.js
+++ index.js
@@ -97,3 +97,21 @@ function convertScoreToGrade(score) {
 
 var output = convertScoreToGrade(91);
 console.log(output);
+
+// 1.8
+function getLongestOfThreeWords(word1, word2, word3) {
+    var longestWord = '';
+
+    if (word1.length >= word2.length && word1.length >= word3.length) {
+        longestWord = word1;
+    } else if (word2.length >= word1.length && word2.length >= word3.length) {
+        longestWord = word2;
+    } else {
+        longestWord = word3;
+    }
+
+    return longestWord;
+}
+
+var output = getLongestOfThreeWords("these", "three", "words")
+console.log(output);
