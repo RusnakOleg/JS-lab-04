1043aa166f8cff457aa3517de21168d8879a3a13
Oleg Rusnak <oleg.rusnak04@gmail.com>
2024-03-28 23:45:05 +0200

    task 1.10

--- index.js
+++ index.js
@@ -127,3 +127,19 @@ function multiply(num1, num2) {
 
 var output = multiply(4, 7);
 console.log(output);
+
+// 1.10
+function computeSumBetween(num1, num2) {
+    if (num2 < num1) {
+        return 0;
+    }
+
+    var sum = 0;
+    for (var i = num1; i < num2; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+var output = computeSumBetween(2, 5);
+console.log(output);
